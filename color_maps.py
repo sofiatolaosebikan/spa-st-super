37dff2ab69c54940b1bb4164bc8abc34e4d5bbcb
sofiat-olaosebikan <sofiat@aims.edu.gh>
2017-08-28 08:56:41 +0100

    color map for all tie density

--- color_maps.py
+++ color_maps.py
@@ -25,7 +25,7 @@ def proportion(value):
     
 def get_all_proportion(size):
     all_proportion = []
-    for i in range(0,7):
+    for i in range(0,11):
         temp = []
         for j in range(0, 11):
             filename = 'experiments/2/' + str(i) + '_' + str(j) +  '/' + size +'/output.csv'
@@ -41,10 +41,10 @@ def subplot(size):
     #print(prop)
     #plt.figure()
     x = [0.005*j for j in range(0,11)]  # lecturer on the x-axis
-    for i in range(0, 7):
+    for i in range(0, 11):
         y = [0.005*i for _ in range(0,11)]  # student on the y-axis    
         plt.scatter(x, y, c=prop[i], marker='s', s=100, linewidths=0,  edgecolors=None, cmap='gist_heat_r', vmin=0, vmax=100)
-        plt.ylim(-0.005)
+        plt.ylim(-0.005, 0.055)
         plt.xlim(-0.005, 0.055)
         
     
@@ -55,71 +55,71 @@ def subplot(size):
 
 
 
-plt.figure(figsize=(15, 15))
-ax1 = plt.subplot(331)
-ax1.title.set_text('instance size 100')
-subplot('100')
-plt.ylabel('tie density for students')
-
-plt.setp(ax1.get_xticklabels(), visible=False)
-
-ax2 = plt.subplot(332, sharey = ax1)
-ax2.title.set_text('instance size 200')
-subplot('200')
-plt.setp(ax2.get_xticklabels(), visible=False)
-plt.setp(ax2.get_yticklabels(), visible=False)
-
-
-ax3 = plt.subplot(333, sharey = ax1)
-ax3.title.set_text('instance size 300')
-subplot('300')
-plt.setp(ax3.get_xticklabels(), visible=False)
-plt.setp(ax3.get_yticklabels(), visible=False)
-
-
-ax4 = plt.subplot(334, sharey = ax1)
-ax4.title.set_text('instance size 400')
-subplot('400')
-plt.ylabel('tie density for students')
-plt.setp(ax4.get_xticklabels(), visible=False)
-
-ax5 = plt.subplot(335, sharey = ax1)
-ax5.title.set_text('instance size 500')
-subplot('500')
-plt.setp(ax5.get_xticklabels(), visible=False)
-plt.setp(ax5.get_yticklabels(), visible=False)
-
-
-ax6 = plt.subplot(336, sharey=ax1)
-ax6.title.set_text('instance size 600')
-subplot('600')
-plt.setp(ax6.get_xticklabels(), visible=False)
-plt.setp(ax6.get_yticklabels(), visible=False)
-
-ax7 = plt.subplot(337, sharey = ax1)
-ax7.title.set_text('instance size 700')
-subplot('700')
-plt.ylabel('tie density for students')
-plt.xlabel('tie density for lecturers')
-
-ax8 = plt.subplot(338, sharey = ax1, sharex=ax7)
-ax8.title.set_text('instance size 800')
-subplot('800')
-plt.setp(ax8.get_yticklabels(), visible=False)
-plt.xlabel('tie density for lecturers')
-
-ax9 = plt.subplot(339, sharey = ax1, sharex=ax7)
-ax9.title.set_text('instance size 900')
-subplot('900')
-plt.setp(ax9.get_yticklabels(), visible=False)
-plt.xlabel('tie density for lecturers')
-
-
-
-plt.subplots_adjust(bottom=0.1, right=0.82, top=0.9)
-cax = plt.axes([0.85, 0.1, 0.03, 0.8])
-plt.colorbar(cax=cax)
-plt.show()
+#plt.figure(figsize=(15, 15))
+#ax1 = plt.subplot(331)
+#ax1.title.set_text('instance size 100')
+#subplot('100')
+#plt.ylabel('tie density for students')
+#
+#plt.setp(ax1.get_xticklabels(), visible=False)
+#
+#ax2 = plt.subplot(332, sharey = ax1)
+#ax2.title.set_text('instance size 200')
+#subplot('200')
+#plt.setp(ax2.get_xticklabels(), visible=False)
+#plt.setp(ax2.get_yticklabels(), visible=False)
+#
+#
+#ax3 = plt.subplot(333, sharey = ax1)
+#ax3.title.set_text('instance size 300')
+#subplot('300')
+#plt.setp(ax3.get_xticklabels(), visible=False)
+#plt.setp(ax3.get_yticklabels(), visible=False)
+#
+#
+#ax4 = plt.subplot(334, sharey = ax1)
+#ax4.title.set_text('instance size 400')
+#subplot('400')
+#plt.ylabel('tie density for students')
+#plt.setp(ax4.get_xticklabels(), visible=False)
+#
+#ax5 = plt.subplot(335, sharey = ax1)
+#ax5.title.set_text('instance size 500')
+#subplot('500')
+#plt.setp(ax5.get_xticklabels(), visible=False)
+#plt.setp(ax5.get_yticklabels(), visible=False)
+#
+#
+#ax6 = plt.subplot(336, sharey=ax1)
+#ax6.title.set_text('instance size 600')
+#subplot('600')
+#plt.setp(ax6.get_xticklabels(), visible=False)
+#plt.setp(ax6.get_yticklabels(), visible=False)
+#
+#ax7 = plt.subplot(337, sharey = ax1)
+#ax7.title.set_text('instance size 700')
+#subplot('700')
+#plt.ylabel('tie density for students')
+#plt.xlabel('tie density for lecturers')
+#
+#ax8 = plt.subplot(338, sharey = ax1, sharex=ax7)
+#ax8.title.set_text('instance size 800')
+#subplot('800')
+#plt.setp(ax8.get_yticklabels(), visible=False)
+#plt.xlabel('tie density for lecturers')
+#
+#ax9 = plt.subplot(339, sharey = ax1, sharex=ax7)
+#ax9.title.set_text('instance size 900')
+#subplot('900')
+#plt.setp(ax9.get_yticklabels(), visible=False)
+#plt.xlabel('tie density for lecturers')
+#
+#
+#
+#plt.subplots_adjust(bottom=0.1, right=0.82, top=0.9)
+#cax = plt.axes([0.85, 0.1, 0.03, 0.8])
+#plt.colorbar(cax=cax)
+#plt.show()
 
 
     
